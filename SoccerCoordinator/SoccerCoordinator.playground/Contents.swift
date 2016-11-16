414795e2b0adff2eb9fe1d8cfb3679e6185418fe
Dan <Daniel@danielimac.local>
2016-11-16 14:48:51 +0200

    added players

---
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,23 @@
+var players: [[String: Any]] = []
+players.append(["name": "Joe Smith", "heightInInches": 42, "soccerExperience": true, "guardianNames": "Jim and Jan Smith"])
+players.append(["name": "Jill Tanner", "heightInInches": 36, "soccerExperience": true, "guardianNames": "Clara Tanner"])
+players.append(["name": "Bill Bon", "heightInInches": 43, "soccerExperience": true, "guardianNames": "Sara and Jenny Bon"])
+players.append(["name": "Eva Gordon", "heightInInches": 45, "soccerExperience": false, "guardianNames": "Wendy and Mike Gordon"])
+players.append(["name": "Matt Gill", "heightInInches": 40, "soccerExperience": false, "guardianNames": "Charles and Sylvia Gill"])
+players.append(["name": "Kimmy Stein", "heightInInches": 41, "soccerExperience": false, "guardianNames": "Bill and Hillary Stein"])
+players.append(["name": "Sammy Adams", "heightInInches": 45, "soccerExperience": false, "guardianNames": "Jeff Adams"])
+players.append(["name": "Karl Saygan", "heightInInches": 42, "soccerExperience": true, "guardianNames": "Heather Bledsoe"])
+players.append(["name": "Suzane Greenberg", "heightInInches": 44, "soccerExperience": true, "guardianNames": "Henrietta Dumas"])
+players.append(["name": "Sal Dali", "heightInInches": 41, "soccerExperience": false, "guardianNames": "Gala Dali"])
+players.append(["name": "Joe Kavalier", "heightInInches": 39, "soccerExperience": false, "guardianNames": "Sam and Elaine Kavalier"])
+players.append(["name": "Ben Finkelstein", "heightInInches": 44, "soccerExperience": false, "guardianNames": "Aaron and Jill Finkelstein"])
+players.append(["name": "Diego Soto", "heightInInches": 41, "soccerExperience": true, "guardianNames": "Robin and Sarika Soto"])
+players.append(["name": "Chloe Alaska", "heightInInches": 47, "soccerExperience": false, "guardianNames": "David and Jamie Alaska"])
+players.append(["name": "Arnold Willis", "heightInInches": 43, "soccerExperience": false, "guardianNames": "Claire Willis"])
+players.append(["name": "Phillip Helm", "heightInInches": 44, "soccerExperience": true, "guardianNames": "Thomas Helm and Eva Jones"])
+players.append(["name": "Les Clay", "heightInInches": 42, "soccerExperience": true, "guardianNames": "Wynonna Brown"])
+players.append(["name": "Herschel Krustofski", "heightInInches": 45, "soccerExperience": true, "guardianNames": "Hyman and Rachel Krustofski"])
+
+for player in players {
+    
+}

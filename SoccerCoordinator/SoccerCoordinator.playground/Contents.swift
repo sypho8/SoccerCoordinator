def3884d2f21b0edb22315b46bcb12c52c50091c
Dan <Daniel@danielimac.local>
2016-11-17 18:36:22 +0200

    added personalized letter

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -1,3 +1,4 @@
+//
 var players: [[String: Any]] = []
 players.append(["name": "Joe Smith", "heightInInches": 42, "soccerExperience": true, "guardianNames": "Jim and Jan Smith"])
 players.append(["name": "Jill Tanner", "heightInInches": 36, "soccerExperience": true, "guardianNames": "Clara Tanner"])
@@ -18,15 +19,16 @@ players.append(["name": "Phillip Helm", "heightInInches": 44, "soccerExperience"
 players.append(["name": "Les Clay", "heightInInches": 42, "soccerExperience": true, "guardianNames": "Wynonna Brown"])
 players.append(["name": "Herschel Krustofski", "heightInInches": 45, "soccerExperience": true, "guardianNames": "Hyman and Rachel Krustofski"])
 
-let numberOfTeams = 3
+//Initializing teams arrays of dictionaries (A players is represented as a dictionary)
+var teamDragons: [[String: Any]] = []
+var teamSharks: [[String: Any]] = []
+var teamRaptors: [[String: Any]] = []
 
-var teamNum1: [[String: Any]] = []
-var teamNum2: [[String: Any]] = []
-var teamNum3: [[String: Any]] = []
-
+//
 var teamIndicatorWithExp = 1
 var teamIndicatorWithoutExp = 1
 
+//
 for player in players {
     
     let playerIsExperienced = player["soccerExperience"] as! Bool
@@ -34,12 +36,12 @@ for player in players {
     if playerIsExperienced {
         
         switch teamIndicatorWithExp {
-        case 1: teamNum1.append(player)
-        case 2: teamNum2.append(player)
-        case 3: teamNum3.append(player)
+        case 1: teamDragons.append(player)
+        case 2: teamSharks.append(player)
+        case 3: teamRaptors.append(player)
         default:
-            teamNum1.append(player)
             teamIndicatorWithExp = 1
+            teamDragons.append(player)
         }
         
         teamIndicatorWithExp += 1
@@ -47,16 +49,45 @@ for player in players {
     } else {
         
         switch teamIndicatorWithoutExp {
-        case 1: teamNum1.append(player)
-        case 2: teamNum2.append(player)
-        case 3: teamNum3.append(player)
+        case 1: teamDragons.append(player)
+        case 2: teamSharks.append(player)
+        case 3: teamRaptors.append(player)
         default:
-            teamNum1.append(player)
             teamIndicatorWithoutExp = 1
+            teamDragons.append(player)
         }
         
         teamIndicatorWithoutExp += 1
         
     }
     
+}
+
+//Generating a personalized letter to the guardians
+func generateLetter(forPlayer player: [String: Any], inTeam team: String) -> String {
+    
+    let playerName = player["name"] as! String
+    let playerGuardians = player["guardianNames"] as! String
+    var teamPracticeDateAndTime = ""
+    
+    switch team {
+    case "Dragons": teamPracticeDateAndTime = "March 17, 1pm"
+    case "Sharks": teamPracticeDateAndTime = "March 17, 3pm"
+    case "Raptors": teamPracticeDateAndTime = "March 18, 1pm"
+    default: teamPracticeDateAndTime = "No practice for specified team"
+    }
+    
+    return "Hi \(playerGuardians), my name is Daniel and I'm \(playerName)'s coach.\nI'm happy to inform you that \(playerName) has been placed on team \(team)!\nThe team's first practice will take place on \(teamPracticeDateAndTime), see you then!"
+    
+}
+
+//Iterating through all three teams of players in order to generate a personalized letters to their guardians
+for player in teamDragons {
+    print(generateLetter(forPlayer: player, inTeam: "Dragons"))
+}
+for player in teamSharks {
+    print(generateLetter(forPlayer: player, inTeam: "Sharks"))
+}
+for player in teamRaptors {
+    print(generateLetter(forPlayer: player, inTeam: "Raptors"))
 }
